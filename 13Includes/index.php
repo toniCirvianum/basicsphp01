f29026e4666de67f12ef30884ddba4fc82383f33
Toni Fernandez <toni.fernandez@cirvianum.cat>
2024-09-17 13:02:07 +0200

    includes començat

---
+++ 13Includes/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Incldues en PHP</title>
+</head>
+<body>
+    <div class="header">
+        <h1>Includes en PHP</h1>
+        <ul>
+            <li><a href="index.php">Inici</a></li>
+            <li><a href="index.php">Sobre mi</a></li>
+            <li><a href="index.php">Contacte</a></li>
+        </ul>
+        <hr>
+    </div>
+    <!-- Contingut-->
+    <div>
+        <h2>Pagina d'inici</h2>
+        <p>Text de prova</p>
+        <hr>
+    </div>
+
+    <footer>
+        <p>Tots els drets reservat &copy Toni Fernandez <?php echo date('Y'); ?></p>
+    </footer>
+    
+</body>
+</html>
